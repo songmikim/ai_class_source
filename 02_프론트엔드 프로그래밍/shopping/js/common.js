233c94e9c6a510ab788408e5ea4d427faf6335a7
songmikim <ksm0200@gmail.com>
2025-04-04 15:36:56 +0900

    주말 과제

---
+++ 02_프론트엔드 프로그래밍/shopping/js/common.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", function(){
+    new Swiper(".main-banner", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        loop: true,
+      });
+});
