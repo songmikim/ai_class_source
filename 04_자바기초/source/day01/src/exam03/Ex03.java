d07baed85bdfbc0f372012bff52b0760b4f85306
songmikim <ksm0200@gmail.com>
2025-04-18 16:24:47 +0900

    ''

---
+++ 04_자바기초/source/day01/src/exam03/Ex03.java
@@ -0,0 +1,13 @@
+package exam03;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        char ch1 = 'A'; // 65
+        char ch2 = 'B'; // 66
+        System.out.println(ch1 + ch2);  // 131
+
+        char ch3 = '가';
+        char ch4 = '나';
+        System.out.println(ch3 + ch4);  // 89240
+    }
+}
---
+++ 04_자바기초/source/day01/src/exam03/Ex04.java
@@ -0,0 +1,9 @@
+package exam03;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        final int NUM = 100;    // 상수
+
+
+    }
+}
